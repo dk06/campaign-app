895938b04b171337e02c921b05c786e2b501c9fa
Deepak <it.cloud33@gmail.com>
2017-11-25 15:53:49 +0530

    no change

--- campaign-app/shared/cuberootService.js
+++
@@ -1,9 +0,0 @@
-app.factory('cuberootService', function($http) {
-  return {
-    async: function() {
-      return $http.get('http://205.147.101.67:8080/marketingv1/getChannelCampaignId?channel_type=facebook&access_token=aaasssdff', function(res){
-        return res;
-      });  //1. this returns promise
-    }
-  };
-});
--- campaign-app/shared/shareBaseUrl.js
+++ campaign-app/shared/shareBaseUrl.js
@@ -5,7 +5,7 @@ app.service('shareBaseUrl', function ($window) {
             var params = {};
                 params.token = $window.localStorage.accessToken;
                 params.userId = $window.localStorage.userId;
-                params.BaseUrl = 'http://campaign-apps.herokuapp.com/api/';
+                params.BaseUrl = window.location.origin + /api/;
                 params.cuberootBaseUrl = "http://205.147.101.67:8080/marketingv1/";
             return params;
         }
